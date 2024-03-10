99bb95cf9fe078945948e0b40f69708dca10fc8d
Lenar Mirzayanov <melkiy-dude01@yandex.ru>
2024-03-10 11:15:59 +0300

    init

---
+++ src/app/views/home/main/main.component.ts
@@ -0,0 +1,82 @@
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {map, Subject, Subscription} from "rxjs";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {PopupComponent} from "../../../shared/components/popup/popup.component";
+import {environment} from "../../../../environments/environment";
+
+// import * as bootstrap from "bootstrap";
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit, OnDestroy, AfterViewInit {
+
+  private subject: Subject<number>;
+
+  constructor(private modalService: NgbModal) {
+
+    this.subject = new Subject<number>();
+    let count = 0;
+    const interval = setInterval(() => {
+      this.subject.next(count++);
+    }, 1000)
+
+    const timeout1 = setTimeout(() => {
+      this.subject.complete();
+    }, 4000)
+  }
+
+  private subscription: Subscription | null = null;
+
+  // @ViewChild('popup')
+  // popup!:TemplateRef<ElementRef>;
+
+  ngOnInit(): void {
+    console.log(environment.production)
+    // const myModalAlternative = new bootstrap.Modal('#myModal', {})
+    // myModalAlternative.show()
+
+    this.subscription = this.subject
+      .subscribe({
+        next: (param: number) => {
+          console.log('subscriber 1: ', param)
+        },
+        error: (err: string) => {
+          console.log('error blyat ' + err)
+        }
+      })
+  }
+
+  @ViewChild(PopupComponent)
+  private popupComponent!: PopupComponent;
+
+  ngAfterViewInit() {
+    this.popupComponent.open();
+
+    // this.modalService.open(this.popup,{});
+
+    // const modalRef = this.modalService.open(PopupComponent);
+    // modalRef.componentInstance.data = 'Main Component'
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+  }
+
+  test() {
+
+
+    // this.subject
+    //   .pipe(
+    //     map((number) => {
+    //       return 'Число: ' + number;
+    //     })
+    //   )
+    //   .subscribe((param: string) => {
+    //     console.log('subscriber 2: ', param)
+    //   })
+
+  }
+}
---
+++ src/app/views/products/product/product.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {ProductService} from "../../../shared/services/product.service";
+import {ProductType} from "../../../../types/product.type";
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+  product: ProductType = {} as ProductType;
+
+  constructor(private productService: ProductService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) {
+
+
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['id']) {
+
+        this.productService.getProduct(+params['id'])
+          ?.subscribe({
+            next: (data) => {
+              this.product = data
+            },
+            error: (error) => {
+             this.router.navigate(['/'])
+            }
+          });
+      }
+    })
+  }
+
+}
